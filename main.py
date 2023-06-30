554cc9663cd8e80d98914645f2e4fd78fd5d8c7f
Orestes Bastos <orestesmkb@gmail.com>
2023-06-30 16:01:37 -0300

    Modified use of findall to get only hunk numbers to use as filter before checking for smells via metrics in database

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+import json
 
 import pandas as pd
 
@@ -50,59 +51,59 @@ try:
         postgreSQL_select_Query = "SELECT * FROM class WHERE project = %s"
         cursor.execute(postgreSQL_select_Query, (project_csv,))
         record = cursor.fetchall()
-        for path in record:
-            split_paths = path[1].split('/', 6)
-            split_path = split_paths[-1]
-            if file_path_csv in split_path:
-                hunk = re.findall("@@ (.*) @@", row['patch'])
-                print(path)
-                print(hunk)
+        for case in record:
+            split_paths = case[1].split('/', 6)
+            path = split_paths[-1]
+            if file_path_csv in path:
+                hunks = re.findall("@@ -(.*),(.*) [+](.*),(.*) @@", row['patch'])
+                for hunk in hunks:
+                    line_number = case[7]
+                    first_start = int(hunk[0])
+                    first_end = int(hunk[0]) + int(hunk[1])
+                    second_start = int(hunk[2])
+                    second_end = int(hunk[2]) + int(hunk[3])
+                    if first_start <= line_number <= first_end or second_start <= line_number <= second_end:
+                        # Get metrics for code smells
+                        metrics = case[9]
+                        smell_LCOM = metrics['PercentLackOfCohesion']
+                        smell_NOF = metrics['CountDeclClassVariable'] + metrics['CountDeclInstanceVariable']
+                        smell_NOM = metrics['CountDeclMethod']
+                        smell_NOPM = metrics['CountDeclMethodPublic']
+                        smell_WMC = metrics['SumCyclomaticModified']
+                        smell_NC = metrics['CountClassDerived']
+                        smell_LOC = metrics['CountLine']
+                        smell_CC = metrics['AvgCyclomatic']
 
-    # Fetch result
-    postgreSQL_select_Query = "SELECT * FROM public.class"
-    cursor.execute(postgreSQL_select_Query)
-    record = cursor.fetchone()
+                        # Create original smells results with false as default before testing
+                        smells_results = {
+                            'MultifacetedAbstraction': False,
+                            'UnnecessaryAbstraction': False,
+                            'InsufficientModularization': False,
+                            'WideHierarchy': False,
+                            'LongMethod': False,
+                            'ComplexMethod': False
+                        }
 
-    # Get metrics for code smells
-    metrics = record[9]
-    smell_LCOM = metrics['PercentLackOfCohesion']
-    smell_NOF = metrics['CountDeclClassVariable'] + metrics['CountDeclInstanceVariable']
-    smell_NOM = metrics['CountDeclMethod']
-    smell_NOPM = metrics['CountDeclMethodPublic']
-    smell_WMC = metrics['SumCyclomaticModified']
-    smell_NC = metrics['CountClassDerived']
-    smell_LOC = metrics['CountLine']
-    smell_CC = metrics['Cyclomatic']
+                        # Individual test for each smell
+                        if smell_LCOM >= 80 and smell_NOF >= 7 and smell_NOM >= 7:
+                            smells_results['MultifacetedAbstraction'] = True
+                        if smell_NOF >= 5 and smell_NOM == 0:
+                            smells_results['UnnecessaryAbstraction'] = True
+                        if smell_NOPM >= 20 or smell_NOM >= 30 or smell_WMC >= 100:
+                            smells_results['InsufficientModularization'] = True
+                        if smell_NC >= 10:
+                            smells_results['WideHierarchy'] = True
+                        if smell_LOC >= 100:
+                            smells_results['LongMethod'] = True
+                        if smell_CC >= 8:
+                            smells_results['ComplexMethod'] = True
 
-    # Create original smells results with false as default before testing
-    smells_results = {
-        'MultifacetedAbstraction': False,
-        'UnnecessaryAbstraction': False,
-        'InsufficientModularization': False,
-        'WideHierarchy': False,
-        'LongMethod': False,
-        'ComplexMethod': False
-    }
+                        smells_json = json.dumps(smells_results)
+                        print(smells_json)
 
-    # Individual test for each smell
-    if smell_LCOM >= 80 and smell_NOF >= 7 and smell_NOM >= 7:
-        smells_results['MultifacetedAbstraction'] = True
-    if smell_NOF >= 5 and smell_NOM == 0:
-        smells_results['UnnecessaryAbstraction'] = True
-    if smell_NOPM >= 20 or smell_NOM >= 30 or smell_WMC >= 100:
-        smells_results['InsufficientModularization'] = True
-    if smell_NC >= 10:
-        smells_results['WideHierarchy'] = True
-    if smell_LOC >= 100:
-        smells_results['LongMethod'] = True
-    if smell_CC >= 8:
-        smells_results['ComplexMethod'] = True
-
-    postgreSQL_alter_Query = "UPDATE TABLE public.class SET smells %s"
-    cursor.execute(postgreSQL_alter_Query, (smells_results,))
-    connection.commit()
-
-    print(smells_results)
+                        postgreSQL_alter_Query = "UPDATE class SET smells = %(obj)s WHERE id %s"
+                        cursor.execute(postgreSQL_alter_Query, (smells_json, case[0],))
+                        connection.commit()
 
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
