b01d9d25c6711d994f8c3ebb586789f63bfddf33
Orestes Bastos <orestesmkb@gmail.com>
2023-07-10 15:28:01 -0300

    initial commit on new branch that substitutes pandas data frame with csv reader dictionary

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+import csv
 import re
-import json
-
-import pandas as pd
 
 import psycopg2
 from psycopg2 import Error
-
 from tqdm.auto import tqdm
 
-df = pd.read_csv('projects_patches.csv')
+# Removing data frame to check if iterating over it slows down the code
+# df = pd.read_csv('projects_patches.csv')
 
 bug_fix_tuple = ()
 smells_results_tuple = ()
@@ -37,110 +35,111 @@ def if_not_exist_create_column(table_name, column_name, data_type):
 
 try:
 
-    # Loop for all rows in the csv file
-    for index, row in tqdm(df.iterrows()):
-        project_csv = row['project']
-        file_path_csv = row['file_path']
-        # split_path_csv = file_path_csv.split('/')
-        # path_csv = split_path_csv[-1]
+    # Opening the csv file as a dictionary
+    with open('projects_patches.csv', mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-        # Fetch classes if it's the same project name and file path as in the csv row
-        postgreSQL_select_Query = "SELECT * FROM public.class WHERE project = %s AND position(%s in file)>0"
-        cursor.execute(postgreSQL_select_Query, (project_csv, file_path_csv))
-        classes = cursor.fetchall()
+        # Loop for all rows in the csv file
+        for row in tqdm(csv_reader):
+            project_csv = row['project']
+            file_path_csv = row['file_path']
 
-        # Loop for all classes in the database with the same project as the current csv row
-        for case in tqdm(classes):
-            split_paths = case[1].split('/', 6)
-            path = split_paths[-1]
+            # Fetch classes if it's the same project name and file path as in the csv row
+            postgreSQL_select_Query = "SELECT * FROM public.class WHERE project = %s AND position(%s in file)>0"
+            cursor.execute(postgreSQL_select_Query, (project_csv, file_path_csv))
+            classes = cursor.fetchall()
 
-            # Check if the file path in the csv row is contained in the current csv row path
-            # if file_path_csv in path:
-            # Fetch methods if it's the same class id as the id of the class in case
-            class_id = case[0]
-            postgreSQL_select_Query = "SELECT * FROM public.method WHERE class_id = %s"
-            cursor.execute(postgreSQL_select_Query, (class_id,))
-            methods = cursor.fetchall()
+            # Loop for all classes in the database with the same project as the current csv row
+            for case in tqdm(classes):
+                split_paths = case[1].split('/', 6)
+                path = split_paths[-1]
 
-            # Findall to get only the numbers from all hunks, but separates in different tuples inside the list
-            hunks = re.findall("@@ -(.*),(.*) [+](.*),(.*) @@", row['patch'])
+                # Fetch methods if it's the same class id as the id of the class in case
+                class_id = case[0]
+                postgreSQL_select_Query = "SELECT * FROM public.method WHERE class_id = %s"
+                cursor.execute(postgreSQL_select_Query, (class_id,))
+                methods = cursor.fetchall()
 
-            # Create or reset default bug fix flag
-            bug_fix_flag = False
+                # Findall to get only the numbers from all hunks, but separates in different tuples inside the list
+                hunks = re.findall("@@ -(.*),(.*) [+](.*),(.*) @@", row['patch'])
 
-            # Create or reset default smells results with all false as default before testing
-            smells_results = {
-                'MultifacetedAbstraction': False,
-                'UnnecessaryAbstraction': False,
-                'InsufficientModularization': False,
-                'WideHierarchy': False,
-                'LongMethod': False,
-                'ComplexMethod': False
-            }
+                # Create or reset default bug fix flag
+                bug_fix_flag = False
 
-            for hunk in hunks:
-                # get line number from class and compare with hunk intervals
-                class_line_number = case[7]
-                first_start = int(hunk[0])
-                first_end = int(hunk[0]) + int(hunk[1])
-                second_start = int(hunk[2])
-                second_end = int(hunk[2]) + int(hunk[3])
+                # Create or reset default smells results with all false as default before testing
+                smells_results = {
+                    'MultifacetedAbstraction': False,
+                    'UnnecessaryAbstraction': False,
+                    'InsufficientModularization': False,
+                    'WideHierarchy': False,
+                    'LongMethod': False,
+                    'ComplexMethod': False
+                }
 
-                # Check if line number in class table from database is within hunk intervals
-                if first_start <= class_line_number <= first_end or second_start <= class_line_number <= second_end:
+                for hunk in hunks:
+                    # get line number from class and compare with hunk intervals
+                    class_line_number = case[7]
+                    first_start = int(hunk[0])
+                    first_end = int(hunk[0]) + int(hunk[1])
+                    second_start = int(hunk[2])
+                    second_end = int(hunk[2]) + int(hunk[3])
 
-                    # Change bug_fix flag if line number is within hunk intervals
-                    bug_fix_flag = True
+                    # Check if line number in class table from database is within hunk intervals
+                    if first_start <= class_line_number <= first_end or second_start <= class_line_number <= second_end:
 
-                    # Get metrics for code smells
-                    class_metrics = case[9]
-                    smell_LCOM = class_metrics['PercentLackOfCohesion']
-                    smell_NOF = class_metrics['CountDeclClassVariable'] + class_metrics['CountDeclInstanceVariable']
-                    smell_NOM = class_metrics['CountDeclMethod']
-                    smell_NOPM = class_metrics['CountDeclMethodPublic']
-                    smell_WMC = class_metrics['SumCyclomaticModified']
-                    smell_NC = class_metrics['CountClassDerived']
+                        # Change bug_fix flag if line number is within hunk intervals
+                        bug_fix_flag = True
 
-                    # Individual test for each smell
-                    if smell_LCOM >= 80 and smell_NOF >= 7 and smell_NOM >= 7:
-                        smells_results['MultifacetedAbstraction'] = True
-                    if smell_NOF >= 5 and smell_NOM == 0:
-                        smells_results['UnnecessaryAbstraction'] = True
-                    if smell_NOPM >= 20 or smell_NOM >= 30 or smell_WMC >= 100:
-                        smells_results['InsufficientModularization'] = True
-                    if smell_NC >= 10:
-                        smells_results['WideHierarchy'] = True
+                        # Get metrics for code smells
+                        class_metrics = case[9]
+                        smell_LCOM = class_metrics['PercentLackOfCohesion']
+                        smell_NOF = class_metrics['CountDeclClassVariable'] + class_metrics['CountDeclInstanceVariable']
+                        smell_NOM = class_metrics['CountDeclMethod']
+                        smell_NOPM = class_metrics['CountDeclMethodPublic']
+                        smell_WMC = class_metrics['SumCyclomaticModified']
+                        smell_NC = class_metrics['CountClassDerived']
 
-                    for method in methods:
-                        method_line_number = method[7]
+                        # Individual test for each smell
+                        if smell_LCOM >= 80 and smell_NOF >= 7 and smell_NOM >= 7:
+                            smells_results['MultifacetedAbstraction'] = True
+                        if smell_NOF >= 5 and smell_NOM == 0:
+                            smells_results['UnnecessaryAbstraction'] = True
+                        if smell_NOPM >= 20 or smell_NOM >= 30 or smell_WMC >= 100:
+                            smells_results['InsufficientModularization'] = True
+                        if smell_NC >= 10:
+                            smells_results['WideHierarchy'] = True
 
-                        # Check if line number in method table from database is also within hunk intervals
-                        if first_start <= method_line_number <= first_end \
-                                or second_start <= method_line_number <= second_end:
+                        for method in methods:
+                            method_line_number = method[7]
 
-                            method_metrics = method[9]
+                            # Check if line number in method table from database is also within hunk intervals
+                            if first_start <= method_line_number <= first_end \
+                                    or second_start <= method_line_number <= second_end:
 
-                            if 'CountLine' in method_metrics:
-                                smell_LOC = method_metrics['CountLine']
-                            else:
-                                smell_LOC = 0
-                            if 'Cyclomatic' in method_metrics:
-                                smell_CC = method_metrics['Cyclomatic']
-                            else:
-                                smell_CC = 0
+                                method_metrics = method[9]
 
-                            if smell_LOC >= 100:
-                                smells_results['LongMethod'] = True
-                            if smell_CC >= 8:
-                                smells_results['ComplexMethod'] = True
+                                if 'CountLine' in method_metrics:
+                                    smell_LOC = method_metrics['CountLine']
+                                else:
+                                    smell_LOC = 0
+                                if 'Cyclomatic' in method_metrics:
+                                    smell_CC = method_metrics['Cyclomatic']
+                                else:
+                                    smell_CC = 0
 
-            if counter_success >= 100:
-                df['bug_fix'] = pd.Series(bug_fix_tuple)
-                df['smells'] = pd.Series(smells_results)
-                print(df)
-                raise Exception('Assigned counter limit reached')
-            if bug_fix_flag:
-                counter_success += 1
+                                if smell_LOC >= 100:
+                                    smells_results['LongMethod'] = True
+                                if smell_CC >= 8:
+                                    smells_results['ComplexMethod'] = True
+
+                if counter_success >= 100:
+                    # Removed data frame
+                    # df['bug_fix'] = pd.Series(bug_fix_tuple)
+                    # df['smells'] = pd.Series(smells_results)
+                    # print(df)
+                    raise Exception('Assigned counter limit reached')
+                if bug_fix_flag:
+                    counter_success += 1
 
 except (Exception, Error) as error:
     print("Error while connecting to PostgreSQL", error)
