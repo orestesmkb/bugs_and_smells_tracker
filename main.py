6c402d4f06582d91baec348cff37cae56d4b390e
Orestes Bastos <orestesmkb@gmail.com>
2023-07-11 15:41:06 -0300

    Modified initial query to check for the csv file path substring in the database file path, got code from development branch

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import re
 import json
-
-from tqdm.auto import tqdm
+import re
 
 import pandas as pd
-
 import psycopg2
 from psycopg2 import Error
+from tqdm.auto import tqdm
 
 df = pd.read_csv('projects_patches.csv')
 print(df)
@@ -54,15 +52,13 @@ try:
         project_csv = row['project']
         file_path_csv = row['file_path']
 
-        # Fetch classes if it's the same project name as in the csv row
-        postgreSQL_select_Query = 'SELECT * FROM public.class WHERE project = %s'
-        cursor.execute(postgreSQL_select_Query, (project_csv,))
+        # Fetch classes if it's the same project name and file path as in the csv row
+        postgreSQL_select_Query = "SELECT * FROM public.class WHERE project = %s AND position(%s in file)>0"
+        cursor.execute(postgreSQL_select_Query, (project_csv, file_path_csv))
         classes = cursor.fetchall()
 
         # Loop for all classes in the database with the same project as the current csv row
         for case in classes:
-            split_paths = case[1].split('/', 6)
-            path = split_paths[-1]
 
             # Set or reset bug fix flag as false for each case
             bug_fix_flag = False
@@ -77,115 +73,114 @@ try:
                 'ComplexMethod': False
             }
 
-            # Check if the file path in the csv row is contained in the current csv row path
-            if file_path_csv in path:
-                # Fetch methods if it's the same class id as the id of the class in case
-                class_id = case[0]
-                postgreSQL_select_Query = 'SELECT * FROM public.method WHERE class_id = %s'
-                cursor.execute(postgreSQL_select_Query, (class_id,))
-                methods = cursor.fetchall()
+            # Fetch methods if it's the same class id as the id of the class in case
+            class_id = case[0]
+            postgreSQL_select_Query = 'SELECT * FROM public.method WHERE class_id = %s'
+            cursor.execute(postgreSQL_select_Query, (class_id,))
+            methods = cursor.fetchall()
 
-                # Findall to get only the numbers from all hunks, but separates in different tuples inside the list
-                hunks = re.findall("@@ -(.*),(.*) [+](.*),(.*) @@", row['patch'])
+            # Findall to get only the numbers from all hunks, but separates in different tuples inside the list
+            hunks = re.findall("@@ -(.*),(.*) [+](.*),(.*) @@", row['patch'])
 
-                for hunk in hunks:
-                    # get line number from class and compare with hunk intervals
-                    class_line_number = case[7]
-                    first_start = int(hunk[0])
-                    first_end = int(hunk[0]) + int(hunk[1])
-                    second_start = int(hunk[2])
-                    second_end = int(hunk[2]) + int(hunk[3])
+            for hunk in hunks:
+                # get line number from class and compare with hunk intervals
+                class_line_number = case[7]
+                first_start = int(hunk[0])
+                first_end = int(hunk[0]) + int(hunk[1])
+                second_start = int(hunk[2])
+                second_end = int(hunk[2]) + int(hunk[3])
 
-                    # Check if line number in class table from database is within hunk intervals
-                    if first_start <= class_line_number <= first_end or second_start <= class_line_number <= second_end:
+                # Check if line number in class table from database is within hunk intervals
+                if first_start <= class_line_number <= first_end or second_start <= class_line_number <= second_end:
 
-                        # Set bug fix flag as true if line number within intervals
-                        bug_fix_flag = True
+                    # Set bug fix flag as true if line number within intervals
+                    bug_fix_flag = True
 
-                        # Get metrics for code smells
-                        class_metrics = case[9]
+                    # Get metrics for code smells
+                    class_metrics = case[9]
 
-                        if 'CountLine' in class_metrics:
-                            smell_LCOM = class_metrics['PercentLackOfCohesion']
-                        else:
-                            smell_LCOM = 0
+                    if 'CountLine' in class_metrics:
+                        smell_LCOM = class_metrics['PercentLackOfCohesion']
+                    else:
+                        smell_LCOM = 0
 
-                        if 'CountLine' in class_metrics:
-                            smell_NOF = class_metrics['CountDeclClassVariable'] + class_metrics['CountDeclInstanceVariable']
-                        else:
-                            smell_NOF = 0
+                    if 'CountLine' in class_metrics:
+                        smell_NOF = class_metrics['CountDeclClassVariable'] + class_metrics['CountDeclInstanceVariable']
+                    else:
+                        smell_NOF = 0
 
-                        if 'CountLine' in class_metrics:
-                            smell_NOM = class_metrics['CountDeclMethod']
-                        else:
-                            smell_NOM = 0
+                    if 'CountLine' in class_metrics:
+                        smell_NOM = class_metrics['CountDeclMethod']
+                    else:
+                        smell_NOM = 0
 
-                        if 'CountLine' in class_metrics:
-                            smell_NOPM = class_metrics['CountDeclMethodPublic']
-                        else:
-                            smell_NOPM = 0
+                    if 'CountLine' in class_metrics:
+                        smell_NOPM = class_metrics['CountDeclMethodPublic']
+                    else:
+                        smell_NOPM = 0
 
-                        if 'CountLine' in class_metrics:
-                            smell_WMC = class_metrics['SumCyclomaticModified']
-                        else:
-                            smell_WMC = 0
+                    if 'CountLine' in class_metrics:
+                        smell_WMC = class_metrics['SumCyclomaticModified']
+                    else:
+                        smell_WMC = 0
 
-                        if 'CountLine' in class_metrics:
-                            smell_NC = class_metrics['CountClassDerived']
-                        else:
-                            smell_NC = 0
+                    if 'CountLine' in class_metrics:
+                        smell_NC = class_metrics['CountClassDerived']
+                    else:
+                        smell_NC = 0
 
-                        # Individual test for each smell
-                        if smell_LCOM >= 80 and smell_NOF >= 7 and smell_NOM >= 7:
-                            smells_results['MultifacetedAbstraction'] = True
+                    # Individual test for each smell
+                    if smell_LCOM >= 80 and smell_NOF >= 7 and smell_NOM >= 7:
+                        smells_results['MultifacetedAbstraction'] = True
 
-                        if smell_NOF >= 5 and smell_NOM == 0:
-                            smells_results['UnnecessaryAbstraction'] = True
+                    if smell_NOF >= 5 and smell_NOM == 0:
+                        smells_results['UnnecessaryAbstraction'] = True
 
-                        if smell_NOPM >= 20 or smell_NOM >= 30 or smell_WMC >= 100:
-                            smells_results['InsufficientModularization'] = True
+                    if smell_NOPM >= 20 or smell_NOM >= 30 or smell_WMC >= 100:
+                        smells_results['InsufficientModularization'] = True
 
-                        if smell_NC >= 10:
-                            smells_results['WideHierarchy'] = True
+                    if smell_NC >= 10:
+                        smells_results['WideHierarchy'] = True
 
-                        for method in methods:
-                            method_line_number = method[7]
+                    for method in methods:
+                        method_line_number = method[7]
 
-                            # Check if line number in method table from database is also within hunk intervals
-                            if first_start <= method_line_number <= first_end \
-                                    or second_start <= method_line_number <= second_end:
+                        # Check if line number in method table from database is also within hunk intervals
+                        if first_start <= method_line_number <= first_end \
+                                or second_start <= method_line_number <= second_end:
 
-                                # # Update method table marking as a bug fix commit
-                                # method_id = method[0]
-                                # postgreSQL_alter_Query = "UPDATE public.method SET bug_fix = %s WHERE id = %s"
-                                # cursor.execute(postgreSQL_alter_Query, (True, method_id))
-                                # connection.commit()
+                            # # Update method table marking as a bug fix commit
+                            # method_id = method[0]
+                            # postgreSQL_alter_Query = "UPDATE public.method SET bug_fix = %s WHERE id = %s"
+                            # cursor.execute(postgreSQL_alter_Query, (True, method_id))
+                            # connection.commit()
 
-                                method_metrics = method[9]
+                            method_metrics = method[9]
 
-                                if 'CountLine' in method_metrics:
-                                    smell_LOC = method_metrics['CountLine']
-                                else:
-                                    smell_LOC = 0
-                                if 'Cyclomatic' in method_metrics:
-                                    smell_CC = method_metrics['Cyclomatic']
-                                else:
-                                    smell_CC = 0
+                            if 'CountLine' in method_metrics:
+                                smell_LOC = method_metrics['CountLine']
+                            else:
+                                smell_LOC = 0
+                            if 'Cyclomatic' in method_metrics:
+                                smell_CC = method_metrics['Cyclomatic']
+                            else:
+                                smell_CC = 0
 
-                                if smell_LOC >= 100:
-                                    smells_results['LongMethod'] = True
-                                if smell_CC >= 8:
-                                    smells_results['ComplexMethod'] = True
+                            if smell_LOC >= 100:
+                                smells_results['LongMethod'] = True
+                                
+                            if smell_CC >= 8:
+                                smells_results['ComplexMethod'] = True
 
-                # Update class table marking as a bug fix commit
-                postgreSQL_alter_Query = 'UPDATE public.class SET bug_fix = %s WHERE id = %s'
-                cursor.execute(postgreSQL_alter_Query, (bug_fix_flag, class_id))
-                connection.commit()
+            # Update class table marking as a bug fix commit
+            postgreSQL_alter_Query = 'UPDATE public.class SET bug_fix = %s WHERE id = %s'
+            cursor.execute(postgreSQL_alter_Query, (bug_fix_flag, class_id))
+            connection.commit()
 
-                # Update database with the test for smells results
-                postgreSQL_alter_Query = 'UPDATE public.class SET smells = %s WHERE id = %s'
-                cursor.execute(postgreSQL_alter_Query, (json.dumps(smells_results), class_id))
-                connection.commit()
+            # Update database with the test for smells results
+            postgreSQL_alter_Query = 'UPDATE public.class SET smells = %s WHERE id = %s'
+            cursor.execute(postgreSQL_alter_Query, (json.dumps(smells_results), class_id))
+            connection.commit()
 
             # If it is a bug fix mark as a success
             if bug_fix_flag:
