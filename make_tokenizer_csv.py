08bd71a26c3f918d973d3b1ce170c3c1644718f9
Orestes Bastos <orestesmkb@gmail.com>
2023-07-31 15:35:31 -0300

    Add creation of files for harmful and clean cases

--- make_tokenizer_csv.py
+++ make_tokenizer_csv.py
@@ -2,6 +2,20 @@ import csv
 import pandas as pd
 import psycopg2
 from psycopg2 import Error
+import os
+
+
+def create_folder(folder_path):
+    # This function will check if a folder exists, if it does not it will create one with the path from the input
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+        print("Folder %s created!" % folder_path)
+    else:
+        print("Folder %s already exists" % folder_path)
+
+
+# check whether directory already exists and if it does not, create it
+create_folder('csv files')
 
 # This scrip generates a csv file with all necessary data to generate tokens via tokenizer
 
@@ -19,11 +33,12 @@ bugs = cursor.fetchall()
 
 try:
 
-    with open('bug_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
+    with open('csv files\\bug_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
         fieldnames = ['id', 'language', 'text', 'smells', 'project']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
+        count = 0
 
         for bug in bugs:
             db_id = bug[0]
@@ -33,17 +48,50 @@ try:
             db_project = bug[3]
             if db_id < 0 or db_language == '' or db_content == '' or db_smells == {}:
                 continue
+            count += 1
             writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
+        else:
+            print(' ')
+            print('All bug fix cases sorted, ' + str(count) + ' total cases saved.')
+            print(count)
 
-    with open('not_bug_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
+    with open('csv files\\harmful_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
+        fieldnames = ['id', 'language', 'text', 'smells', 'project']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        count = 0
+
+        for bug in bugs:
+            db_id = bug[0]
+            db_language = bug[2]
+            db_content = bug[8]
+            db_smells = bug[13]
+            db_project = bug[3]
+            if db_id < 0 or db_language == '' or db_content == '' or db_smells == {}:
+                continue
+            if not any(db_smells.values()):
+                continue
+            count += 1
+            writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
+        else:
+            print(' ')
+            print('All harmful cases sorted, ' + str(count) + ' total cases saved.')
+            print(count)
+
+    with open('csv files\\clean_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
         fieldnames = ['id', 'language', 'text', 'smells', 'project']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
         # Get data from csv file
-        df = pd.read_csv('bug_tokenizer_data.csv')
+        df = pd.read_csv('csv files\\harmful_tokenizer_data.csv')
         # Check how many languages there are
         languages = df['language'].unique()
+
+        print(' ')
+        print('Attempting to get same amount of clean cases based on amount of harmful cases:')
+        print(' ')
 
         for language in languages:
             # Get amount for each language
@@ -52,10 +100,10 @@ try:
             # Fetch all cases that are not a bug fix for each language
             postgreSQL_select_Query = "SELECT * FROM public.class WHERE bug_fix = %s and language = %s"
             cursor.execute(postgreSQL_select_Query, ('false', language))
-            not_bugs = cursor.fetchall()
+            clean = cursor.fetchall()
             count = 0
 
-            for case in not_bugs:
+            for case in clean:
                 smells = case[13]
                 if any(smells.values()):
                     continue
@@ -70,15 +118,18 @@ try:
                     writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
                     count += 1
                     if count == lang_size:
+                        print('Amount of cases for ' + language + ' reached, ' + str(count) + ' total cases saved.')
                         break
             else:
-                print('Not enough cases for ' + language + ', only ' + str(count) + ' total cases.')
+                print('Not enough cases for ' + language + ', only ' + str(count) + ' total cases saved.')
 
 except (Exception, Error) as error:
+    print(' ')
     print('Error while connecting to PostgreSQL', error)
 
 finally:
     if connection:
         cursor.close()
+        print(' ')
         print('PostgreSQL connection is closed')
         connection.close()
