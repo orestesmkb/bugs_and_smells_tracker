d0cf9d111ffbe3be160f4b6e2736dd573465cc8b
Orestes Bastos <orestesmkb@gmail.com>
2023-07-31 16:08:47 -0300

    Now creating a file for all bug fix cases that have no smells, based on the amout of harmful cases

--- make_tokenizer_csv.py
+++ make_tokenizer_csv.py
@@ -52,8 +52,7 @@ try:
             writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
         else:
             print(' ')
-            print('All bug fix cases sorted, ' + str(count) + ' total cases saved.')
-            print(count)
+            print('All bug fix cases sorted, ' + str(count) + ' total cases.')
 
     with open('csv files\\harmful_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
         fieldnames = ['id', 'language', 'text', 'smells', 'project']
@@ -72,12 +71,11 @@ try:
                 continue
             if not any(db_smells.values()):
                 continue
-            count += 1
             writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
+            count += 1
         else:
             print(' ')
-            print('All harmful cases sorted, ' + str(count) + ' total cases saved.')
-            print(count)
+            print('All harmful cases sorted, ' + str(count) + ' total cases.')
 
     with open('csv files\\clean_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
         fieldnames = ['id', 'language', 'text', 'smells', 'project']
@@ -104,24 +102,66 @@ try:
             count = 0
 
             for case in clean:
-                smells = case[13]
-                if any(smells.values()):
+                db_id = case[0]
+                db_language = case[2]
+                db_content = case[8]
+                db_smells = case[13]
+                db_project = case[3]
+                if db_id < 0 or db_language == '' or db_content == '' or db_smells == {}:
                     continue
-                else:
-                    db_id = case[0]
-                    db_language = case[2]
-                    db_content = case[8]
-                    db_smells = case[13]
-                    db_project = case[3]
-                    if db_id < 0 or db_language == '' or db_content == '' or db_smells == {}:
-                        continue
-                    writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
-                    count += 1
-                    if count == lang_size:
-                        print('Amount of cases for ' + language + ' reached, ' + str(count) + ' total cases saved.')
-                        break
+                if any(db_smells.values()):
+                    continue
+                writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
+                count += 1
+                if count == lang_size:
+                    print('Amount of cases for ' + language + ' reached, ' + str(count) + ' total cases.')
+                    break
             else:
-                print('Not enough cases for ' + language + ', only ' + str(count) + ' total cases saved.')
+                print('Not enough cases for ' + language + ', there is a total of ' + str(lang_size) +
+                      ' cases, but only ' + str(count) + ' total cases correspond to the filter.')
+
+    with open('csv files\\bug_without_smells_tokenizer_data.csv', 'w', encoding="utf-8", newline='') as csvfile:
+        fieldnames = ['id', 'language', 'text', 'smells', 'project']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        # Get data from csv file
+        df = pd.read_csv('csv files\\harmful_tokenizer_data.csv')
+        # Check how many languages there are
+        languages = df['language'].unique()
+
+        print(' ')
+        print('Attempting to get same amount of bug fix cases without smells based on amount of harmful cases:')
+        print(' ')
+
+        for language in languages:
+            # Get amount for each language
+            lang_size = len(df[df["language"] == language])
+
+            # Fetch all cases that are not a bug fix for each language
+            postgreSQL_select_Query = "SELECT * FROM public.class WHERE bug_fix = %s and language = %s"
+            cursor.execute(postgreSQL_select_Query, ('true', language))
+            clean = cursor.fetchall()
+            count = 0
+
+            for case in clean:
+                db_id = case[0]
+                db_language = case[2]
+                db_content = case[8]
+                db_smells = case[13]
+                db_project = case[3]
+                if db_id < 0 or db_language == '' or db_content == '' or db_smells == {}:
+                    continue
+                if any(db_smells.values()):
+                    continue
+                writer.writerow({'id': db_id, 'language': db_language, 'text': db_content, 'smells': db_smells, 'project': db_project})
+                count += 1
+                if count == lang_size:
+                    print('Amount of cases for ' + language + ' reached, ' + str(count) + ' total cases.')
+                    break
+            else:
+                print('Not enough cases for ' + language + ', there is a total of ' + str(lang_size) +
+                      ' cases, but only ' + str(count) + ' total cases correspond to the filter.')
 
 except (Exception, Error) as error:
     print(' ')
