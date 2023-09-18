f738b784e7c156b2cef3f92315c9e9194363c1da
Orestes Bastos <orestesmkb@gmail.com>
2023-09-18 16:54:56 -0300

    Add comments to create_folder.py explaining its function

--- create_folders.py
+++ create_folders.py
@@ -2,7 +2,14 @@ import os
 
 
 def create_folders(folder_path='csv files', additional_folders=None):
-    # This function will check if a folder exists, if it does not it will create one with the path from the input
+    """
+    This function will check if a folder exists, if it does not it will create one with the path from the input, if a
+    tuple of additional folders is defined, they are also created inside the main folder.
+
+    :param folder_path: Path to main folder to be created
+    :param additional_folders: Tuple of strings with the name of the sub-folders to be created, if none are defined none
+    are created
+    """
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
         if additional_folders is not None:
