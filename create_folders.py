72c9865cd28fa1b1691c820bd279a73733ed5f46
Orestes Bastos <orestesmkb@gmail.com>
2023-10-16 10:52:04 -0300

    Modified create_folders to create subfolders even if the main folder already exists and inform the user

--- create_folders.py
+++ create_folders.py
@@ -12,10 +12,15 @@ def create_folders(folder_path='csv files', additional_folders=None):
     """
     if not os.path.exists(folder_path):
         os.mkdir(folder_path)
-        if additional_folders is not None:
-            for additional_folder in additional_folders:
-                additional_folder_path = os.path.join(folder_path, additional_folder)
-                os.mkdir(additional_folder_path)
         print("Folder %s created!" % folder_path)
     else:
         print("Folder %s already exists" % folder_path)
+
+    if additional_folders is not None:
+        for additional_folder in additional_folders:
+            additional_folder_path = os.path.join(folder_path, additional_folder)
+            if not os.path.exists(additional_folder_path):
+                os.mkdir(additional_folder_path)
+                print("Folder %s created!" % additional_folder_path)
+            else:
+                print("Folder %s already exists" % additional_folder_path)
